0d18eba28911b92a42f33523edf1ff3d0fcb09a3
BXIF <53058745+BXIF@users.noreply.github.com>
2019-07-19 15:24:12 -0400

    Add files via upload

--- Pot Faders/pot_fader_buzzer_part2.ino
+++ Pot Faders/pot_fader_buzzer_part2.ino
@@ -1,39 +1,16 @@
-/*
-  Analog input, analog output, serial output
-
-  Reads an analog input pin, maps the result to a range from 0 to 255 and uses
-  the result to set the pulse width modulation (PWM) of an output pin.
-  Also prints the results to the Serial Monitor.
-
-  The circuit:
-  - potentiometer connected to analog pin 0.
-    Center pin of the potentiometer goes to the analog pin.
-    side pins of the potentiometer go to +5V and ground
-  - LED connected from digital pin 9 to ground
-
-  created 29 Dec. 2008
-  modified 9 Apr 2012
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogInOutSerial
-*/
-
-//int Buzzer = 2;//connect buzzer to pin 2
 
 
 // These constants won't change. They're used to give names to the pins used:
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 const int analogOutPin = 9; // Analog output pin that the LED is attached to
 //buzzer
-const int analogOutPinBuzzer = 2; // Analog output pin that the LED is attached to
+const int analogOutPinBuzzer = 10; // Analog output pin that the LED is attached to
 
 
 int sensorValue = 0;        // value read from the pot
 int outputValue = 0;        // value output to the PWM (analog out)
-int buzzerFrequency;
-int i;
+int buzzerFrequency = 0;
+//int i;
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -50,16 +27,11 @@ void loop() {
   // change the analog out value:
   analogWrite(analogOutPin, outputValue);
 
-  for(int i=0; i<=255; i++){
-    buzzerFrequency= map(outputValue, 0, 255, 0, 700);
-    tone(analogOutPinBuzzer, buzzerFrequency);
-  }
+  buzzerFrequency= map(outputValue, 0, 255, 0, 1000);
+  tone(analogOutPinBuzzer, buzzerFrequency); 
 
   Serial.print("\t buzzerF = ");
   Serial.println(buzzerFrequency);
-  
 
-  // wait 2 milliseconds before the next loop for the analog-to-digital
-  // converter to settle after the last reading:
-  delay(1000);
+  //delay(1000);
 }
