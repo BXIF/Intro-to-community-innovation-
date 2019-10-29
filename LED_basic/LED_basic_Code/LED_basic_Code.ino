643a56238dbec6dcd575f2444585d0e10b8dc283
Bronx Innovation Factory <53058745+BXIF@users.noreply.github.com>
2019-10-29 10:20:46 -0400

    Update LED_basic_Code.ino

--- LED_basic/LED_basic_Code/LED_basic_Code.ino
+++ LED_basic/LED_basic_Code/LED_basic_Code.ino
@@ -7,7 +7,7 @@ pinMode(LED1, OUTPUT);
 
 void loop() {
  digitalWrite (LED1, HIGH);
- delay(3000)
- ;digitalWrite(LED1, LOW);
- delay(4000)
-;}
+ delay(3000);
+ digitalWrite(LED1, LOW);
+ delay(3000);
+}
